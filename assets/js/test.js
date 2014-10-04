5b7bfe37e6c9d56d8724749f8c216a56df99dedd
Mathew Jennings <mathew.percussion@gmail.com>
2014-10-04 14:54:39 -0400

    Added javascript file with code that can call the HP API

---
+++ assets/js/test.js
@@ -0,0 +1,22 @@
+
+
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
+
+function test() {
+	var request = makeHttpObject();
+	request.open("GET", "https://api.idolondemand.com/1/api/sync/analyzesentiment/v1?text=I+am+really+annoyed+with+your+poor+performance+recently&language=eng&apikey=d00696e1-ec9b-44e1-8265-591b852e5f0f", true);
+	request.send(null);
+	request.onreadystatechange = function() {
+	  if (request.readyState == 4)
+	    alert(request.responseText);
+	};
+}
