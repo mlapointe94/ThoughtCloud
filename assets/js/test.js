6043540cb254b75646aaa84f7a8ec2edc9c155cc
Mathew Jennings <mathew.percussion@gmail.com>
2014-10-04 17:02:41 -0400

    Added information on how to use the various API calls we want from IDOL OnDemand

--- assets/js/test.js
+++ assets/js/test.js
@@ -26,3 +26,18 @@ function test2() {
 	    document.getElementById("result_text").innerHTML = request.responseText;
 	};
 }
+
+// get sentiment analysis on a string
+//"https://api.idolondemand.com/1/api/sync/analyzesentiment/v1?text=INSERT+TEXT+HERE&language=eng&apikey=d00696e1-ec9b-44e1-8265-591b852e5f0f"
+
+//categorize a string
+//"https://api.idolondemand.com/1/api/sync/categorizedocument/v1?text=INSERT+TEXT+HERE&apikey=d00696e1-ec9b-44e1-8265-591b852e5f0f"
+//apparently only works with wikipedia and the news. Can we make it work with our Parse database?
+
+//entity extraction
+//"https://api.idolondemand.com/1/api/sync/extractentities/v1?text=INSERT+TEXT+HERE&entity_type=ENTITY_TYPE&apikey=d00696e1-ec9b-44e1-8265-591b852e5f0f"
+//ENTITY TYPES: https://www.idolondemand.com/developer/apis/extractentities#request
+
+//expand terms
+//"https://api.idolondemand.com/1/api/sync/expandterms/v1?text=INSERT+TEXT+HERE&expansion=EXPANSION_TYPE&sort=SORT_TYPE&apikey=d00696e1-ec9b-44e1-8265-591b852e5f0f"
+//EXPANSION AND SORT TYPES: https://www.idolondemand.com/developer/apis/expandterms#request
