15350013765adb90bf1e6d84ed69953af185fd1d
Mathew Jennings <mathew.percussion@gmail.com>
2014-10-04 15:56:15 -0400

    Working on page-change+api-call. Isn't right yet.

--- assets/js/test.js
+++ assets/js/test.js
@@ -12,11 +12,17 @@ function makeHttpObject() {
 }
 
 function test() {
+	console.log("Page change");
+	location.href = 'results.html';
+}
+
+function test2() {
+	console.log("Page load");
 	var request = makeHttpObject();
 	request.open("GET", "https://api.idolondemand.com/1/api/sync/analyzesentiment/v1?text=I+am+really+annoyed+with+your+poor+performance+recently&language=eng&apikey=d00696e1-ec9b-44e1-8265-591b852e5f0f", true);
 	request.send(null);
 	request.onreadystatechange = function() {
 	  if (request.readyState == 4)
-	    alert(request.responseText);
+	    document.getElementById("result_text").innerHTML = request.responseText;
 	};
 }
